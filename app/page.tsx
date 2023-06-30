8b0f02bcd860851cb1acb21624e67ced961a3507
Moein Moeinnia <moeinnia@protonmail.com>
2023-06-30 14:48:32 +0430

    structure added and not finished yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+'use client';
 import Image from 'next/image'
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_TEST);
+
   return (
     <main className="">
+      <h1>yyyyy</h1>
     </main>
   )
 }
+

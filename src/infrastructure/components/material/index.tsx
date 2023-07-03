36966ffba17ccf1276e991231e2c5a2d1325a6a8
Moeinmn <moeinnia@protonmail.com>
2023-07-03 11:50:59 +0330

    theme provide addded for mui5

---
+++ src/infrastructure/components/material/index.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider, useTheme ,createTheme } from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+interface IBaseMaterial {
+    children: any
+}
+
+// const generateClassName = createGenerateClassName({
+//     productionPrefix: "plotSet",
+// });
+
+const MaterialTheme = (props: IBaseMaterial) => {
+    const theme = useTheme();
+    // const currentTheme = createTheme({
+    //     ...themeComposer(theme.currentTheme === "dark" ? dark as PaletteOptionsCustom : light as PaletteOptionsCustom)
+    // });
+    return (
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {props.children}
+            </ThemeProvider>
+    );
+};
---
+++ src/infrastructure/components/material/themes/dark.ts
@@ -0,0 +1,49 @@
+
+const dark: any  = {
+    type: "dark",
+    primary: {
+        light: "#4085d7",
+        main: "#0771ed",
+        dark: "#0157bd"
+    },
+    secondary: {
+        light: "#ee9733",
+        main: "#ed8307",
+        dark: "#c6710d"
+    },
+    grey: {
+        "100": "#2f2f2f",
+        "200": "#5e5e5e",
+        "300": "#979797",
+        "400": "#cbcbcb",
+        "500": "#e6e6e6",
+        "600": "#f2f2f2",
+        "700": "#f9f9f9"
+    },
+    error: {
+        main: "#c30052",
+        light: "#ed2e7e",
+        contrastText: "#fff2f7"
+    },
+    success: {
+        main: "#00966d",
+        light: "#00ba88",
+        contrastText: "#f3fdfa"
+    },
+    warning: {
+        main: "#daa43d",
+        light: "#f4b740",
+        contrastText: "#fff8e1"
+    },
+    info: {
+        main: "#168fdf",
+        light: "#18a0ff",
+        contrastText: "#ebf7ff"
+    },
+    text: {
+        primary: "#2F2F2F",
+        secondary: "#8F8F8F"
+    }
+};
+
+export default dark;

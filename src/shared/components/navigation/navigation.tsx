e1b3f73fea490507d97a790279a434c1acd94916
Moeinmn <moeinnia@protonmail.com>
2023-07-09 16:56:42 +0330

    some styling changes

--- src/shared/components/navigation/navigation.tsx
+++ src/shared/components/navigation/navigation.tsx
@@ -12,26 +12,26 @@ const Navigation:FC = () =>{
 							{/* Website Logo */}
 							<a href="#" className="flex items-center py-4 px-2">
 								{/* <Image src="logo.png" alt="Logo" className="h-8 w-8 mr-2"> */}
-								<span className="font-semibold text-gray-500 text-lg">Navigation</span>
+								<span className="font-semibold text-gray text-lg">Navigation</span>
 							</a>
 						</div>
 						{/* Primary Navbar items */}
 						<div className="hidden md:flex items-center space-x-1">
-							<a href="" className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">Home</a>
-							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Services</a>
-							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">About</a>
-							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
+							<a href="" className="py-4 px-2 text-primary border-b-4 border-primary font-semibold ">Home</a>
+							<a href="" className="py-4 px-2 text-gray font-semibold hover:text-primary transition duration-300">Services</a>
+							<a href="" className="py-4 px-2 text-gray font-semibold hover:text-primary transition duration-300">About</a>
+							<a href="" className="py-4 px-2 text-gray font-semibold hover:text-primary transition duration-300">Contact Us</a>
 						</div>
 					</div>
 					{/* Secondary Navbar items */}
 					<div className="hidden md:flex items-center space-x-3 ">
-						<a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
-						<a href="" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
+						<a href="" className="py-2 px-2 font-medium text-gray rounded hover:bg-primary hover:text-white transition duration-300">Log In</a>
+						<a href="" className="py-2 px-2 font-medium text-white bg-primary rounded hover:bg-primary-400 transition duration-300">Sign Up</a>
 					</div>
 					{/* Mobile menu button */}
 					<div className="md:hidden flex items-center">
 						<button className="outline-none mobile-menu-button">
-						{/* <svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+						{/* <svg className=" w-6 h-6 text-gray hover:text-primary "
 							x-show="!showMenu"
 							fill="none"
 							stroke-linecap="round"
@@ -49,10 +49,10 @@ const Navigation:FC = () =>{
 			{/* mobile menu */}
 			<div className="hidden mobile-menu">
 				<ul className="">
-					<li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-					<li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-					<li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-					<li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+					<li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-primary font-semibold">Home</a></li>
+					<li><a href="#services" className="block text-sm px-2 py-4 hover:bg-primary transition duration-300">Services</a></li>
+					<li><a href="#about" className="block text-sm px-2 py-4 hover:bg-primary transition duration-300">About</a></li>
+					<li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-primary transition duration-300">Contact Us</a></li>
 				</ul>
 			</div>
 			<div className="h-full border-2 border-gray-200

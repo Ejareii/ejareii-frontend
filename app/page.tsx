d4ecc915b05f19480382aa08b167571b11d0ae40
Moein Moeinnia <moeinnia@protonmail.com>
2023-08-14 22:33:04 +0430

    landing card component tried to add

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,26 @@ export default function Home() {
   console.log(process.env.NEXT_PUBLIC_TEST);
 
   return (
-    // <Grid container height="100vh" alignItems="center" justifyContent="center" direction="column">
-    <div className="h-full border-2 border-gray-200
-                border-opacity-60 rounded-lg
-                bg-primary
-                overflow-hidden">
- 
-        <div className="p-6 hover:bg-accent
-                    hover:text-white transition
-                    duration-300 ease-in">
- 
-            <h1 className="text-2xl font-semibold mb-3">
-                Hover
-            </h1>
+    <>
+    <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
         </div>
-    </div>
-      
+    </>
   );
 }

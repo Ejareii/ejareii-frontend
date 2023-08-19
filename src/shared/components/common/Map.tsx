ccf024ecc221f6b1789751fec55c3488ffc1a1cd
Moeinmn <moeinnia@protonmail.com>
2023-08-19 13:25:49 +0330

    new tile layer added

--- src/shared/components/common/Map.tsx
+++ src/shared/components/common/Map.tsx
@@ -21,7 +21,12 @@ interface MapProps {
 }
 
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const url2 = "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+const JwagSunnyURL = 'https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}'
+
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const attribution2 = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+const JwagSunnyAtribution = '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
@@ -32,8 +37,9 @@ const Map: React.FC<MapProps> = ({ center }) => {
         className="h-[35vh] rounded-lg"
       >
         <TileLayer
-          url={url}
-          attribution={attribution}
+          url={JwagSunnyURL}
+          attribution={JwagSunnyAtribution}
+          accessToken='PyTJUlEU1OPJwCJlW1k0NC8JIt2CALpyuj7uc066O7XbdZCjWEL3WYJIk6dnXtps'
         />
         {center && (
           <Marker position={center as L.LatLngExpression} />

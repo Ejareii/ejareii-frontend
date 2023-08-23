f0e426b871ae6d13550177ec3a2873a29bd5f366
mahdiyar <mahdiyar@pro.ai>
2023-08-23 14:16:22 +0100

    add signup form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { MainLayout } from '@/src/shared/components/layouts/main-layout'
 import localFont from '@next/font/local'
 import './globals.css'
+import RegisterModal from '@/src/shared/components/modals/RegisterModal'
+import ToasterProvider from '@/src/providers/ToasterProvider'
+
 
 const myFont = localFont({ src: '../fonts/IRANYekanBold.ttf' })
 
@@ -12,6 +15,9 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">      
       <body className={myFont.className}>
+        
+        <ToasterProvider />
+        <RegisterModal/>
         <MainLayout>
         </MainLayout>
         <div >
---
+++ src/shared/components/modals/RegisterModal.tsx
@@ -0,0 +1,149 @@
+'use client';
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+// import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { 
+  FieldValues, 
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
+
+
+import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../common/Heading";
+import Button from "../common/Button";
+import useRegisterModal from "@/src/hooks/useRegisterModal";
+import useLoginModal from "@/src/hooks/useLoginModal";
+
+const RegisterModal= () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { 
+    register, 
+    handleSubmit,
+    formState: {
+      errors,
+    },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios.post('/api/register', data)
+    .then(() => {
+      toast.success('Registered!');
+      registerModal.onClose();
+      loginModal.onOpen();
+    })
+    .catch((error) => {
+      toast.error("errror");
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
+  }
+
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal])
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="به اجاره ای خوش آمدید"
+        subtitle="ساختن اکانت!"
+      />
+      <Input
+        id="email"
+        label="ایمیل"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="نام"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="رمز"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="ادامه دادن با گوگل"
+        icon={FcGoogle}
+        onClick={() => console.log('google')} 
+      />
+      <Button 
+        outline 
+        label="ادامه دادن با گیت هاب"
+        icon={AiFillGithub}
+        onClick={() => console.log('github')}
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p> یک حساب کاربری دارید؟
+          <span 
+            onClick={onToggle} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            >ورود</span>
+        </p>
+      </div>
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={true}
+      title="ثبت نام"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+}
+
+export default RegisterModal;

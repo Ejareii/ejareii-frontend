e23ce37288185041fea2669f4952d1dc685d3164
Moeinmn <moeinnia@protonmail.com>
2023-11-04 12:21:03 +0330

    chore: seo related some files added

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,39 @@
 import getListings from "../actions/getListing";
 import SearchComponent from "@/src/modules/search";
+import StructuredData from "@/src/shared/components/common/StructuredData";
 import { RentalEntity } from "@/src/shared/dtos/rental.dto";
-
+import { WithContext, Product } from "schema-dts";
 
 interface ISearchRentlas {
   category?: string;
-  lat?:number;
-  lng?:number;
-  zoom?:number;
+  lat?: number;
+  lng?: number;
+  zoom?: number;
 }
 
-const SearchPage = async ({ searchParams } : { searchParams: ISearchRentlas }) => {
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: ISearchRentlas;
+}) => {
   const listings = await getListings(searchParams);
+
+  //NOT WORKING WELL IN SERVER COMPONENTS
+  // const schema: WithContext<Product> = {
+  //   "@context": "https://schema.org",
+  //   "@type": "Product",
+  //   color: "red",
+  //   description: "very good rental",
+  // };
+
   return (
-    <SearchComponent listings={listings?.map((list: any) => new RentalEntity(list))}/>
-  )
-}
+    <>
+      {/* <StructuredData data={schema} /> */}
+      <SearchComponent
+        listings={listings?.map((list: any) => new RentalEntity(list))}
+      />
+    </>
+  );
+};
 
 export default SearchPage;
